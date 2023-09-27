b1008ba5a679f9a09bd8a4993332a39fb06bd2c6
basha97 <bashaadil2727@gmail.com>
2023-09-27 11:20:04 +0530

    feat: modified place order widget

--- lib/features/food_item/presentation/widgets/place_order.dart
+++ lib/features/food_item/presentation/widgets/place_order.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:plateron_app/features/food_item/presentation/providers/food_item_provider.dart';
 
-class PlaceOrderWidget extends StatelessWidget {
+class PlaceOrderWidget extends HookConsumerWidget {
   const PlaceOrderWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      bottom: true,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        height: 50,
-        width: double.infinity,
-        color: Colors.grey.shade100,
-        child: Row(
-          children: [
-            Icon(Icons.shopping_cart_outlined, size: 14),
-            const SizedBox(width: 4),
-            Text('2 items'),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Place Order',
-                style: TextStyle(color: Colors.white),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final foodItemListProvider = ref.watch(foodItemProvider);
+    return foodItemListProvider.isEmpty
+        ? const SizedBox()
+        : SafeArea(
+            bottom: true,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 50,
+              width: double.infinity,
+              color: Colors.grey.shade100,
+              child: Row(
+                children: [
+                  Icon(Icons.shopping_cart_outlined, size: 14),
+                  const SizedBox(width: 4),
+                  Text('${foodItemListProvider.length} items'),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Place Order',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
+                    ),
+                  )
+                ],
               ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
