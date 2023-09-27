da15c6d5de15c9c0bdf77297b147c98710d8b1f7
basha97 <bashaadil2727@gmail.com>
2023-09-27 15:24:32 +0530

    feat: added unit test `calculateTotalAmount`

--- test/main_test.dart
+++ test/main_test.dart
@@ -39,4 +39,34 @@ void main() {
         imageUrl: '');
     expect(findItemInList(addedItems, notInList), notInList);
   });
+
+  test('calculateTotalAmount', () {
+    List<FoodItem> addedItems = [
+      FoodItem(
+          id: '1',
+          name: 'Item 1',
+          price: 10.0,
+          quantity: 2,
+          description: '',
+          imageUrl: ''),
+      FoodItem(
+          id: '2',
+          name: 'Item 2',
+          price: 20.0,
+          quantity: 1,
+          description: '',
+          imageUrl: ''),
+    ];
+
+    String totalAmount = calculateTotalAmount(addedItems);
+    expect(totalAmount, '40.00');
+  });
+}
+
+String calculateTotalAmount(List<FoodItem> items) {
+  double totalAmount = 0.0;
+  for (var item in items) {
+    totalAmount += item.price * (item.quantity ?? 1);
+  }
+  return totalAmount.toStringAsFixed(2);
 }
